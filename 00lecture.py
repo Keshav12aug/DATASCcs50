cd14c31752ae809ad0358cc5cb60277136697fa0
Keshav12aug <keshavdubey12aug@gmail.com>
2025-05-27 00:59:33 +0530

    adding for the day

---
+++ 00lecture.py
@@ -0,0 +1,24 @@
+#Ask for the username
+name = input("What's your name ? ").capitalize().strip()
+
+'''
+greet the user #comment style
+'''
+#greet the user
+
+# to capitalise
+
+#name = name.capitalize()
+
+#name = name.strip()
+
+print(f"hello, {name}")
+
+
+
+
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,15 @@
+rent = 1150
+gas = 110
+groceries = 5600
+
+#to check rent 
+
+print(rent)
+
+#to calculate the total expenses 
+
+total = rent + gas + groceries
+
+print(total)
+
+
