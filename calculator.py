5bc56254794c2a2a0656ac629d54630b5b27727d
Keshav12aug <keshavdubey12aug@gmail.com>
2025-05-28 02:06:37 +0530

    learnings

---
+++ calculator.py
@@ -0,0 +1,30 @@
+# x = int(input("What's x ? "))
+# y = int(input("What's y ? "))
+
+# z = x + y
+
+x = float(input("What's x ? "))
+y = float(input("What's y ? "))
+
+#z = round(x + y)  ''' for addition and rounding up a number
+
+# or
+
+# print(round(x + y))
+
+# print(f"{z:,}") 
+
+
+'''
+for division
+'''
+
+z = round(x/y, 2) #making the float limited to 2 decimal places 
+
+
+print(z)
+
+# also 
+#print(f"{z:.2f}")
+
+
---
+++ def.py
@@ -0,0 +1,20 @@
+def hello(to):
+    print("hello,", to)
+
+name = input("What's your name? ").capitalize()
+hello(name)
+
+
+'''
+applying this to the calculator.py
+
+def main():
+    x = float(input("What's x ? "))
+    print("x squared is", square(x))
+
+def square(n):
+     return n * n
+
+main()
+
+'''
