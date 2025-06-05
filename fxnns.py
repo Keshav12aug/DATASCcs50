fb30010b1dac15813481ec2a82c6fa98019329be
Keshav12aug <keshavdubey12aug@gmail.com>
2025-06-05 22:01:21 +0530

    avg

---
+++ fxnns.py
@@ -0,0 +1,22 @@
+'''def average():
+
+    a = int(input("Enter the value of a : "))
+    b = int(input("Enter the value of b : "))
+    c = int(input("Enter the value of c : "))
+
+    avg = (a + b + c)/3
+
+    print(avg)
+
+average()
+'''
+
+def average(a,b,c):
+    d = (a+b+c)/3.0
+    return d 
+
+o1 = average(3,4,5)
+o2 = average(82,97,64)
+
+print(o1)
+print(o2)
